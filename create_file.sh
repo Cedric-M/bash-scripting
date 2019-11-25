6012162269a43f9e2641558785ad2440ead65976
Cedric-M <cirdec95@gmail.com>
2019-11-25 16:16:54 +0100

    Update: creation of a docx file + added install_pandoc

--- create_file.sh
+++ create_file.sh
@@ -2,8 +2,13 @@
 # This script will ask the name of the folder you want to empty, then erase all the data it may contain and check if it has successfully been emptied.
 # Usage: ./<scriptfilename>
 
+install_pandoc () {
+	echo "We need to install pandoc in order to proceed"
+	read -p "Press "enter" to continue"
+	sudo apt-get -y install pandoc
+}
+
 echo "Which file do you wanna make?"
-echo "Word(.docx) / pdf (.pdf) / Text (.txt)"
 
 PS3='Please enter your choice: '
 options=("Word(.docx)" "Text (.txt)" "Pdf (.pdf)" "Quit")
@@ -11,13 +16,18 @@ select opt in "${options[@]}"
 do
     case $opt in
         "Word(.docx)")
-            echo "you chose choice 1"
+            echo "You chose create a Word file"
+		install_pandoc
+		#> file.docx
+		echo "" | pandoc -o Word.docx
             ;;
         "Text (.txt)")
             echo "you chose choice 2"
+		> MyText.txt
             ;;
         "Pdf (.pdf)")
             echo "you chose choice $REPLY which is $opt"
+		> MyPdf.pdf
             ;;
         "Quit")
             break
@@ -25,3 +35,7 @@ do
         *) echo "invalid option $REPLY";;
     esac
 done
+
+
+
+
