1018370e35a6189982f39303e95e1ae0a0c37598
Cedric-M <cirdec95@gmail.com>
2019-12-17 10:02:36 +0100

    Added script to install docker

---
+++ install-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script will ask install docker on ubuntu https://doc.ubuntu-fr.org/docker
+# Usage: ./<scriptfilename>
+
+wget https://get.docker.com/ -O script-docker.sh
+chmod +x script-docker.sh
+./script-docker.sh
