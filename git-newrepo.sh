0f4b0e5012fb4f5a08d2d113e5a12820f0b5ab32
Cedric-M <cirdec95@gmail.com>
2019-12-18 09:47:12 +0100

    fixed remote add origin, added variables and made a folder

--- git-newrepo.sh
+++ git-newrepo.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # Create a new repository from the command line
 
+mkdir gitfolder
+cd gitfolder
 touch README.md
 git init
 git add README.md
 git commit -m "first commit"
 
-
 echo "Please enter your username:"
 read gitusername
 
 echo "Please enter the name of your repo:"
+echo "Note: make sure you already made the repo in GitHub before continuing."
 read gitreponame
 
-git remote add origin git@github.com:<username>/<reponame>.git
+git remote add origin https://github.com/$gitusername/$gitreponame.git
 git push -u origin master
