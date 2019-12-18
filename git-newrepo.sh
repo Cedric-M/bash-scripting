4e0fc52731191f5d2456eedd3c946093e8e2601d
Cedric-M <cirdec95@gmail.com>
2019-12-18 09:14:51 +0100

    Update: new repo

--- git-newrepo.sh
+++ git-newrepo.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 # Create a new repository from the command line
+
+touch README.md
+git init
+git add README.md
+git commit -m "first commit"
+
+
+echo "Please enter your username:"
+read gitusername
+
+echo "Please enter the name of your repo:"
+read gitreponame
+
+git remote add origin git@github.com:<username>/<reponame>.git
+git push -u origin master
