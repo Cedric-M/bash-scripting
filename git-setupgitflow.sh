ae3cc144ed0e05120ca8e666df827b0c5634cafb
Cedric-M <cirdec95@gmail.com>
2019-12-20 14:39:07 +0100

    Update: push -u origin for each branch

--- git-setupgitflow.sh
+++ git-setupgitflow.sh
@@ -3,12 +3,27 @@
 
 echo "Setting up Gitflow Workflow:"
 
-git checkout develop
+if [ -d "gitfolder/" ] 
+then
+    echo "Directory gitfolder/ exists."
+    cd gitfolder
+	git checkout -b develop
+	git push -u origin develop
 
-git checkout -b feature develop
+	git checkout -b feature develop
+	git push -u origin feature
 
-git checkout -b release develop
+	git checkout -b release develop
+	git push -u origin release
 
-git checkout -b hotfix master
+	git checkout -b hotfix master
+	git push -u origin hotfix
+
+
+else
+    echo "Error: Directory /path/to/dir does not exists."
+fi
+
+
 
 
