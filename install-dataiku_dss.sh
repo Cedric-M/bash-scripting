d05d433669501f9d5088588e26305d51b95377c8
Cedric-M <cirdec95@gmail.com>
2020-01-10 10:02:57 +0100

    Update: typo and comments

--- install-dataiku_dss.sh
+++ install-dataiku_dss.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Dataiku DSS works on Ubuntu, Debian, CentOS, RHEL and Amazon Linux.
+# Script to install Dataiku DSS. Dataiku DSS works on Ubuntu, Debian, CentOS, RHEL and Amazon Linux.
 # Usage: ./<scriptfilename>
 
 #Download Dataiku DSS (https://cdn.downloads.dataiku.com/public/dss/)
@@ -9,9 +9,11 @@ wget https://cdn.downloads.dataiku.com/public/dss/6.0.1/dataiku-dss-6.0.1.tar.gz
 tar xzf dataiku-dss-6.0.1.tar.gz
 
 #Install
-dataiku-dss-6.0.1/installer.sh -d Dataiku-dir -p 11000
+dataiku-dss-6.0.1/installer.sh -d Dataiku_dir -p 11000
 
 #Start
 Dataiku_dir/bin/dss start
 
 #Browse to http://localhost:11000 
+echo "Browse to http://localhost:11000"
+
