f6ece67bc85d6688f14c5459a199927a64be2564
Cedric-M <cirdec95@gmail.com>
2020-01-10 09:51:02 +0100

    Add: install-git

---
+++ install-git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Basic script to install git
+# Usage: ./<scriptfilename>
+
+sudo apt install git
+
+git --version
+
+#git config --global user.email « your@email.com »
+#git config --global user.name « username »
+
